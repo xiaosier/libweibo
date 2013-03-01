2de57896545e4b5717354501d7458c7b3ab8f669
root <root@vm12080024.(none)>
2013-03-01 17:37:48 +0800

    first commit

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define( "WB_AKEY" , 'xxxxxxxxxx' );
+define( "WB_SKEY" , 'xxxxxxxxxxxxxxxxxxxxxxxxx' );
+define( "WB_CALLBACK_URL" , 'http://xxxxxxxxxxxx/callback.php' );
